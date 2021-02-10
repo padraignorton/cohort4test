fcc225c90dc2fc87f5b479cc439d426d2f0a362a
BMcMack <78730400+BMcMack@users.noreply.github.com>
2021-02-10 11:33:16 +0000

    Controller
    Controller working as intended, also gravity and jump function added

--- co-4-t/Assets/Scripts/MouseLook.cs
+++ co-4-t/Assets/Scripts/MouseLook.cs
@@ -24,7 +24,6 @@ public class MouseLook : MonoBehaviour
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        yRotation += mouseX;
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
