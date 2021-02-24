bb076b6ea509218c092aec7536a3f9a14affd3a6
snzzzero <kpopandrews@yahoo.co.uk>
2021-02-24 20:18:34 +0000

    gameplay

---
+++ co-4-t/Assets/Scripts/ReplayMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -3);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Qutting Game...");
+        Application.Quit();
+    }
+}
